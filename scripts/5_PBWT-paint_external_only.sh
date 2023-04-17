533a66ad010d98f0b1988958cc99cc3941faf5f3
Sam Morris <aey472@rescomp1.hpc.in.bmrc.ox.ac.uk>
2023-04-17 16:53:22 +0100

    update scripts

--- scripts/5_PBWT-paint_external_only.sh
+++ scripts/5_PBWT-paint_external_only.sh
@@ -5,7 +5,7 @@
 #SBATCH -e paint_external_only_%j.err
 #SBATCH -p short
 #SBATCH -c 2
-#SBATCH --array 1-22
+#SBATCH --array 1
 
 chr=${SLURM_ARRAY_TASK_ID}
 
@@ -16,28 +16,34 @@ painting_output=/well/ckb/users/aey472/projects/ckb_popgen/data/painting_output
 
 module purge all
 source ~/.bashrc
-module purge all
-mamba activate pbwt
+#module purge all
+#mamba activate pbwt
 
-pbwt \
-	-readVcfGT ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf \
-	-check \
-	-stats \
-	-selectSamples ${ckb_external_data}/external_sample_final_names.txt \
-	-paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf
+#pbwt \
+#	-readVcfGT ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf \
+#	-check \
+#	-stats \
+#	-selectSamples ${ckb_external_data}/external_sample_final_names.txt \
+#	-paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only
 
 
 ## Then we cluster using finestructure ##
 
-#fs fs \
-#        -x 1000000 \
-#        -y 2000000 \
-#        -z 10000 \
-##        ${chunkcounts} ${stem}.mcmc.xml
+mamba activate finestructure
+
+chunkcounts=/well/ckb/users/aey472/projects/ckb_popgen/data/painting_output/sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only.chunkcounts.out
+stem=sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only
+fs_output=/well/ckb/users/aey472/projects/ckb_popgen/data/finestructure_output
+
+fs fs \
+        -x 1000000 \
+        -y 2000000 \
+        -z 10000 \
+        ${chunkcounts} ${fs_output}/${stem}.mcmc.xml
 
 
-#fs fs \
-#        -x 100000 \
-#        -m Tree \
-#        -t 100000 \
-##         ${chunkcounts} ${stem}.mcmc.xml ${stem}.tree.xml
+fs fs \
+        -x 100000 \
+        -m Tree \
+        -t 100000 \
+        ${chunkcounts} ${fs_output}/${stem}.mcmc.xml ${fs_output}/${stem}.tree.xml
