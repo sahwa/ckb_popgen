9ec4abff6c5c6390c9bbcf1702538d383a77dd06
Sam Morris <aey472@rescomp2.hpc.in.bmrc.ox.ac.uk>
2023-04-20 11:16:18 +0100

    update

--- scripts/5_PBWT-paint_external_only.sh
+++ scripts/5_PBWT-paint_external_only.sh
@@ -3,9 +3,9 @@
 #SBATCH -J finestructure_external_only
 #SBATCH -o finestructure_external_only_%j.out
 #SBATCH -e finestructure_external_only_%j.err
-#SBATCH -p long
-#SBATCH -c 1
-#SBATCH --array 1-5
+#SBATCH -p short
+#SBATCH -c 2
+#SBATCH --array 1
 
 chr=${SLURM_ARRAY_TASK_ID}
 
@@ -27,29 +27,44 @@ fs_output=/well/ckb/users/aey472/projects/ckb_popgen/data/finestructure_output
 #	-readVcfGT ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf \
 #	-check \
 #	-stats \
-#	-selectSamples ${ckb_external_data}/external_sample_final_names.txt \
-#	-paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only
+#	-selectSamples ${ckb_external_data}/external_sample_final_names.no_admixed.txt \
+#	-paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only.no_admixed
 
 
 ## Then we cluster using finestructure ##
 
-T=$(sed -n ${SLURM_ARRAY_TASK_ID}'{p;q}' finestructure_T_params.txt)
+#T=$(sed -n ${SLURM_ARRAY_TASK_ID}'{p;q}' finestructure_T_params.txt)
 
-cd ${fs_output}
+#cd ${fs_output}
 
-mamba activate finestructure
+#mamba activate finestructure
 
-chunkcounts=/well/ckb/users/aey472/projects/ckb_popgen/data/painting_output/sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only.chunkcounts.out
-stem=sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only
+chunkcounts=${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only.no_admixed.chunkcounts.out
+stem=sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only.no_admixed
 fs_output=/well/ckb/users/aey472/projects/ckb_popgen/data/finestructure_output
 
 fs=/well/ckb/users/aey472/program_files/finestructure4/fs
 
+#${fs} fs \
+#	-m T \
+#	-T 1 \
+#	-t ${T} \
+#	${painting_output}/${stem}.chunkcounts.out ${stem}.estep20.xml ${stem}.${T}.xm0l
+
+
+#### also try normal finestructure ####
+
+stem=sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.external_only.no_admixed
+
 ${fs} fs \
-	-m T \
-	-T 1 \
-	-t ${T} \
-	${painting_output}/${stem}.chunkcounts.out ${stem}.estep20.xml ${stem}.${T}.xm0l
+        -x 1000000 \
+        -y 2000000 \
+        -z 10000 \
+        ${chunkcounts} ${fs_output}/${stem}.mcmc.xml
 
 
-
+${fs} fs \
+        -x 100000 \
+        -m Tree \
+        -t 100000 \
+         ${chunkcounts} ${fs_output}/${stem}.mcmc.xml ${fs_output}/${stem}.tree.xml
