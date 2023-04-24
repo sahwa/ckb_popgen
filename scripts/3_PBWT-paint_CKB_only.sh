78e1e3f58cc805d9537213bded9a222cf162d374
Sam Morris <aey472@rescomp2.hpc.in.bmrc.ox.ac.uk>
2023-04-24 16:03:46 +0100

    update

--- scripts/3_PBWT-paint_CKB_only.sh
+++ scripts/3_PBWT-paint_CKB_only.sh
@@ -1,38 +1,64 @@
 #!/bin/bash
 #SBATCH -A ckb.prj
-#SBATCH -J pbwt_paint_all
-#SBATCH -o pbwt_paint_all_%j.out
-#SBATCH -e pbwt_paint_all_%j.err
+#SBATCH -J ckb_merge_lengths
+#SBATCH -o ckb_merge_lengths_%j.out
+#SBATCH -e ckb_merge_lengths_%j.err
 #SBATCH -p short
-#SBATCH -c 18
-#SBATCH --array 1-22
+#SBATCH -c 16
 
 # ###########################################################################################
 #	                               Paint just CKB samples against each other                  #
 #############################################################################################
 
-chr=${SLURM_ARRAY_TASK_ID}
+#chr=${SLURM_ARRAY_TASK_ID}
 
-module purge all
-source ~/.bashrc
-module purge all
-mamba activate pbwt
+#module purge all
+#source ~/.bashrc
+#module purge all
+#mamba activate pbwt
 
 external_data=/well/ckb/users/aey472/projects/ckb_popgen/data/external_data
 ckb_external_data=/well/ckb/users/aey472/projects/ckb_popgen/data/CKB_external
 painting_output=/well/ckb/users/aey472/projects/ckb_popgen/data/painting_output
+programs=/well/ckb/users/aey472/projects/ckb_popgen/programs
 
-rel_free=/well/ckb/shared/filesystem/metadata/sample_inclusion_exclusion_lists/relative_free/rel_free.ls
+#rel_free=/well/ckb/shared/filesystem/metadata/sample_inclusion_exclusion_lists/relative_free/rel_free.ls
 
-pbwt \
-	-readVcfGT ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf \
-	-check \
-	-selectSamples ${ckb_external_data}/relfree_local.txt \
-	-stats \
-	-paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree
+#pbwt \
+#	-readVcfGT ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf \
+#	-check \
+#	-selectSamples ${ckb_external_data}/relfree_local.txt \
+#	-stats \
+#	-paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree
 
-rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.regionsquaredchunkcounts.out
-rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.regionchunkcounts.out
+#rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.regionsquaredchunkcounts.out
+#rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.regionchunkcounts.out
 
-gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunkcounts.out
-gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunklengths.out
+#gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunkcounts.out
+#gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunklengths.out
+
+#################### merge #############
+
+cd ${painting_output}
+
+#Rscript ${programs}/merge_chunklengths.R \
+#	-n "/well/ckb/users/aey472/projects/ckb_popgen/data/CKB_external/relfree_local.RC_ID.txt" \
+#	-p "sgdp_hgdp_1kGP_CKB.chr" \
+#	-a ".AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunkcounts.out.gz" \
+#	-c "1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22" \
+#	-o "sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunkcounts.out.gz"
+
+Rscript ${programs}/merge_chunklengths.R \
+  -n "/well/ckb/users/aey472/projects/ckb_popgen/data/CKB_external/relfree_local.RC_ID.txt" \
+  -p "sgdp_hgdp_1kGP_CKB.chr" \
+  -a ".AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunklengths.out.gz" \
+  -c "1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22" \
+  -o "sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunklengths.out.gz"
+
+######### run greedy fs ############
+
+#chunkcounts=${painting_output}/sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.chunkcounts.out
+#programs=/well/ckb/users/aey472/projects/ckb_popgen/programs
+#fs_out=/well/ckb/users/aey472/projects/ckb_popgen/data/finestructure_output
+
+#bash ${programs}/finestructuregreedy.sh ${chunkcounts} ${fs_out}/sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.CKB_only_relfree.greedy.xml
--- scripts/4_PBWT-paint_CKB_external.sh
+++ scripts/4_PBWT-paint_CKB_external.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -A ckb.prj
-#SBATCH -J pbwt_paint_all
-#SBATCH -o pbwt_paint_all_%j.out
-#SBATCH -e pbwt_paint_all_%j.err
+#SBATCH -J ckbexternal_merge_counts
+#SBATCH -o ckbexternal_merge_counts_%j.out
+#SBATCH -e ckbexternal_merge_counts_%j.err
 #SBATCH -p short
-#SBATCH -c 12
-#SBATCH --array 1-22
+#SBATCH -c 16
+#SBATCH --array 1
 
 #############################################################################################
 # Paint all unrelated CKB samples and most external reference samples for use in SOURCEFIND #
@@ -15,34 +15,43 @@ chr=${SLURM_ARRAY_TASK_ID}
 
 module purge all
 source ~/.bashrc
-module purge all
-mamba activate pbwt
+#module purge all
+#mamba activate pbwt
 
 external_data=/well/ckb/users/aey472/projects/ckb_popgen/data/external_data
 ckb_external_data=/well/ckb/users/aey472/projects/ckb_popgen/data/CKB_external
 painting_output=/well/ckb/users/aey472/projects/ckb_popgen/data/painting_output
 programs=/well/ckb/users/aey472/projects/ckb_popgen/programs
 
-pbwt \
-        -readVcfGT ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf \
-        -check \
-        -stats \
-        -paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external
+#pbwt \
+#        -readVcfGT ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf \
+#        -check \
+#        -stats \
+#        -paint ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external
 
-rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.regionsquaredchunkcounts.out
-rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.regionchunkcounts.out
+#rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.regionsquaredchunkcounts.out
+#rm ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.regionchunkcounts.out
 
-gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunkcounts.out
-gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out
+#gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunkcounts.out
+#gzip ${painting_output}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out
 
 ####
 
 #bcftools query -l ${ckb_external_data}/sgdp_hgdp_1kGP_CKB.chr${chr}.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.bcf > ${ckb_external_data}/all_CKB_external.sample_names.txt
 #sample_names=${ckb_external_data}/all_CKB_external.sample_names.txt
 
-#cd ${painting_output}
+cd ${painting_output}
+
+Rscript ${programs}/merge_chunklengths.R \
+       -n "all_CKB_external.sample_names.CKB_RC_ID.txt" \
+       -p "sgdp_hgdp_1kGP_CKB.chr" \
+       -a ".AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunkcounts.out.gz" \
+       -c "1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22" \
+       -o "sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunkcounts.out.gz"
 
 #Rscript ${programs}/merge_chunklengths.R \
-#	${sample_names} \
-#	sgdp_hgdp_1kGP_CKB.chr1.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr2.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr3.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr4.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr5.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr6.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr7.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr8.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr9.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr10.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr11.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr12.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr13.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr14.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr15.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr16.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr17.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr18.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr19.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr20.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr21.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz,sgdp_hgdp_1kGP_CKB.chr22.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz \
-#	test.out
+#  -n "all_CKB_external.sample_names.CKB_RC_ID.txt" \
+#  -p "sgdp_hgdp_1kGP_CKB.chr" \
+#  -a ".AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz" \
+#  -c "1,2,3,4,5,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22" \
+#  -o "sgdp_hgdp_1kGP_CKB.AllChr.AllChr.CKB_snps.GT.no_duplicates.rmdup.conformed.phased.newnames.maf_filter.relfree.local.all_CKB_external.chunklengths.out.gz"
