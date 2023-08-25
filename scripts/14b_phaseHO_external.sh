ed96e8f24ac00ae99094ecf971c5d60b38eb8012
sahwa <ucbtsm8@ucl.ac.uk>
2023-08-25 15:13:38 +0100

    aug 25th

--- scripts/14b_phaseHO_external.sh
+++ scripts/14b_phaseHO_external.sh
@@ -4,7 +4,7 @@
 #SBATCH -o phaseHO_%A_%a.out
 #SBATCH -e phaseHO_%A_%a.err
 #SBATCH -p short
-#SBATCH -c 16
+#SBATCH -c 32
 #SBATCH -a 1-22
 
 source directories.config
@@ -23,12 +23,18 @@ merge_dir=/well/ckb/users/aey472/projects/ckb_popgen/data/HumanOriginsExternal/m
 #sed 's/|/\//g' | \
 #bcftools view -Oz > ${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.vcf.gz
 
-beagle=${programs}/beagle.22Jul22.46e.jar
-java -jar ${beagle} \
-	gt=${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.vcf.gz \
-	chrom=${chr} \
-	map=${gen_maps}/plink.chr${chr}.GRCh38.map \
-	nthreads=16 \
-	out=${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.phased
+module purge all && module load BCFtools/1.17-GCC-12.2.0
+zcat ${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.vcf.gz | bgzip -c > ${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.bgz.vcf.gz
+bcftools index -f ${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.bgz.vcf.gz
+
+gmap=/well/ckb/users/aey472/program_files/shapeit4/maps/chr${chr}.b38.gmap.gz
+
+module purge all && module load SHAPEIT4/4.2.2-foss-2020b
+shapeit4.2 \
+	--input ${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.bgz.vcf.gz \
+	--region ${chr} \
+	--map ${gmap} \
+	--thread 32 \
+	--out ${merge_dir}/WangAncient_Lao_Lipson_Nakatskua_QinSt_Skoglund_WangModern_Kutanan_Viet_1KG_AllChr.missing_filtered.remove_related_duplicates.chr${chr}.CKB.phased
 
 
