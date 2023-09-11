78661409d45997e543938a9a83df5d2aed63f589
sam morris <33362328+sahwa@users.noreply.github.com>
2023-09-11 23:36:29 +0100

    Update src.cpp

--- programs/TVD_perm/src.cpp
+++ programs/TVD_perm/src.cpp
@@ -3,12 +3,10 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 double TVD_cpp(Rcpp::NumericMatrix x) {
-	
 	int x_nrow = x.nrow();
 	Rcpp::NumericVector res(x_nrow * x_nrow, 0.0);
 	Rcpp::NumericVector diff;		
 	int counter = -1;
-
 	for (int i = 0; i<x_nrow; i++) {
 		for (int j=0; j<x_nrow; j++) {
 			counter++;
@@ -18,18 +16,14 @@ double TVD_cpp(Rcpp::NumericMatrix x) {
 			res[counter] = sum;	
 		}
 	}
-
 	double TVD = Rcpp::mean(res);
 	return TVD;
 }
-
-
 
 // [[Rcpp::export]]
 double TVD_cpp_gpt(Rcpp::NumericMatrix x) {
   const int x_nrow = x.nrow();
   Rcpp::NumericVector res(x_nrow * x_nrow, 0.0);
-
   for (int i = 0; i < x_nrow; i++) {
     Rcpp::NumericVector row_i = x.row(i);
     for (int j = 0; j < x_nrow; j++) {
@@ -38,7 +32,6 @@ double TVD_cpp_gpt(Rcpp::NumericMatrix x) {
       res[i * x_nrow + j] = sum;
     }
   }
-
   double TVD = Rcpp::mean(res);
   return TVD;
 }
